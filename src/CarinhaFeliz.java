8270aadfedb2c3242ecc05c508ebebe9dd0d9229
claudneia <164636272+claudneia@users.noreply.github.com>
2024-06-07 20:33:16 -0300

    Add files via upload
    Exercício lição 02

---
+++ src/CarinhaFeliz.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ALUNO
+ */
+public class CarinhaFeliz {
+    public static void main(String[]args){
+ System.out.println("   XXXX  ");
+ System.out.println(" X      X ");
+ System.out.println("X  X  X  X");
+ System.out.println("X        X");
+ System.out.println("X  X  X  X");
+ System.out.println("X   XX   X");
+ System.out.println(" X      X");
+ System.out.println("   XXXX"  );
+ 
+    }
+}
